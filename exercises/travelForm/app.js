f8b2e5592b1dfd740d8c04681ad83a0256b258e9
Casey Radermacher <casey_radermacher@icloud.com>
2019-03-08 17:59:58 -0700

    update

---
+++ exercises/travelForm/app.js
@@ -0,0 +1,28 @@
+document.travelForm.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+    let firstName = document.travelForm.firstName.value;
+    let lastName = document.travelForm.lastName.value;
+    let age = document.travelForm.age.value;
+    let gender = document.travelForm.gender.value;
+    let location = document.travelForm.location;
+    let checkedLocation = []
+        for (let i = 0; i < location.length; i++) {
+            if(location[i].checked){
+                checkedLocation.push(location[i].value)
+            } 
+            
+        }
+//console.log(checkedLocation);
+
+    let restriction = document.travelForm.restriction;
+    let checkedRestriction = []
+        for (let i = 0; i < restriction.length; i++) {
+            if(restriction[i].checked){
+                checkedRestriction.push(restriction[i].value)
+            }
+        
+    }
+console.log(checkedRestriction);
+
+
+});

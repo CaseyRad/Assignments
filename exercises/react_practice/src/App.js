4fd3f682a332ea7795099f28fa21890cabf4c90f
Casey Radermacher <casey_radermacher@icloud.com>
2019-03-21 15:58:33 -0600

    react

---
+++ exercises/react_practice/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './App.css';
+import Header from "./Header.js";
+import Body from "./Body.js";
+
+
+const App = () => {
+return (
+  <div>
+    <Body/>
+    <h1>Hello</h1>
+    <div>
+      React practice
+    </div>
+  </div>
+ )
+img src ""
+}
+
+export default App;
+
---
+++ exercises/react_practice/src/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div>
+            <Banner />
+            <Info />
+            <Products />
+            <img src="" alt=""/>
+        </div>
+    )
+}
+export default Main;
